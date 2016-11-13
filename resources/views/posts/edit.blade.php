7a5888601d1215d1e65e899bbb79a1715a54ff93
magdi <ahmed.magdi@brightcreations.com>
2016-11-13 11:52:04 +0200

    crud posts v1

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layout')
+@section('css')
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
+@endsection
+@section('header')
+    <div class="page-header">
+        <h1><i class="glyphicon glyphicon-edit"></i> Posts / Edit #{{$post->id}}</h1>
+    </div>
+@endsection
+
+@section('content')
+    @include('error')
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <form action="{{ route('posts.update', $post->id) }}" method="POST">
+                <input type="hidden" name="_method" value="PUT">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group @if($errors->has('user_id')) has-error @endif">
+                       <label for="user_id-field">User_id</label>
+                    <input type="text" id="user_id-field" name="user_id" class="form-control" value="{{ is_null(old("user_id")) ? $post->user_id : old("user_id") }}"/>
+                       @if($errors->has("user_id"))
+                        <span class="help-block">{{ $errors->first("user_id") }}</span>
+                       @endif
+                    </div>
+                    <div class="form-group @if($errors->has('body')) has-error @endif">
+                       <label for="body-field">Body</label>
+                    <textarea class="form-control" id="body-field" rows="3" name="body">{{ is_null(old("body")) ? $post->body : old("body") }}</textarea>
+                       @if($errors->has("body"))
+                        <span class="help-block">{{ $errors->first("body") }}</span>
+                       @endif
+                    </div>
+                <div class="well well-sm">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <a class="btn btn-link pull-right" href="{{ route('posts.index') }}"><i class="glyphicon glyphicon-backward"></i>  Back</a>
+                </div>
+            </form>
+
+        </div>
+    </div>
+@endsection
+@section('scripts')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
+  <script>
+    $('.date-picker').datepicker({
+    });
+  </script>
+@endsection
